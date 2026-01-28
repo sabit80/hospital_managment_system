f5fc6d260780dceae2b739b185db437395fc3563
sabit80 <hasib.sabit@gmail.com>
2026-01-29 00:31:32 +0600

    update pom.xml and module-info.java for new dependencies

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,6 +5,7 @@ module com.hms {
 
     opens com.hms to javafx.fxml;
     opens com.hms.controller to javafx.fxml;
+    opens com.hms.views to javafx.fxml;
     
     exports com.hms;
     exports com.hms.controller;
